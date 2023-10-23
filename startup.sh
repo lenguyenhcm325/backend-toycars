0cb7fca27c4f243a6fdfda864f3ba12d0d90be46
Nguyen <lenguyenhcm325@gmail.com>
2023-10-23 20:44:07 +0200

    fix startup.sh to check only for status code 200 from elasticsearch

--- startup.sh
+++ startup.sh
@@ -4,33 +4,15 @@ CHECK_INTERVAL=5
 
 check_elasticsearch() {
   # Use curl to get both the HTTP status code and the response body
-  RESPONSE=$(curl -u $ELASTICSEARCH_USERNAME:$ELASTICSEARCH_PASSWORD --silent --write-out "HTTPSTATUS:%{http_code}" "$ELASTICSEARCH_HOST:$ELASTICSEARCH_PORT/_cluster/health")
-  
-  # Extract the HTTP status code from the response
-  HTTP_STATUS=$(echo $RESPONSE | tr -d '\n' | sed -e 's/.*HTTPSTATUS://')
-
-  # Extract the response body (by removing the HTTP status code from the end)
-  BODY=$(echo $RESPONSE | sed -e 's/HTTPSTATUS\:.*//g')
+  HTTP_STATUS=$(curl -u $ELASTICSEARCH_USERNAME:$ELASTICSEARCH_PASSWORD -s -o /dev/null -w "%{http_code}" "$ELASTICSEARCH_HOST:$ELASTICSEARCH_PORT/_cluster/health")
 
   # If HTTP status code is not 200, Elasticsearch might not be running or reachable
   if [ "$HTTP_STATUS" -ne 200 ]; then
     echo "Elasticsearch returned HTTP status $HTTP_STATUS"
     return 1  # Elasticsearch is not ready
-  fi
-
-  # If the response is valid JSON, parse the status; otherwise, consider Elasticsearch not ready
-  STATUS=$(echo $BODY | jq -r '.status' 2>/dev/null)
-  if [[ $? -ne 0 ]]; then
-    echo "Elasticsearch did not return valid JSON"
-    return 1  # Elasticsearch is not ready
-  fi
-
-  # Check the cluster status
-  if [[ "$STATUS" == "green" || "$STATUS" == "yellow" ]]; then
-    return 0  # Elasticsearch is ready
   else
-    echo "Elasticsearch cluster status is $STATUS"
-    return 1  # Elasticsearch is not ready
+    echo "Elasticsearch is ready"
+    return 0
   fi
 }
 
